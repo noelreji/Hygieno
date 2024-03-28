38ff9cffcab1f3f473c3387f699997c31619abad
cosmicbit <philipsjose2003@gmail.com>
2024-03-28 14:19:17 +0530

    signupv3

---
+++ public/signup/script.js
@@ -0,0 +1,22 @@
+function checkPasswordStrength(password) {
+    // Check for minimum length
+    document.getElementById("lengthCheckbox").checked = password.length >= 8;
+    
+    // Check for presence of uppercase letter
+    document.getElementById("uppercaseCheckbox").checked = /[A-Z]/.test(password);
+    
+    // Check for presence of lowercase letter
+    document.getElementById("lowercaseCheckbox").checked = /[a-z]/.test(password);
+    
+    // Check for presence of number
+    document.getElementById("numberCheckbox").checked = /\d/.test(password);
+    
+    // Check for presence of special character
+    document.getElementById("specialCheckbox").checked = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
+}
+function checkSamePassword(password1,password2) {
+    if(password1==password2){
+        alert("valid");
+    }
+
+}
