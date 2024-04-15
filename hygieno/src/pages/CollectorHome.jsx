74138c65e84fc97658c7d76ea86d24604d7c4ab5
cosmicbit <philipsjose2003@gmail.com>
2024-04-15 19:56:05 +0530

    port corrected

--- hygieno/src/pages/CollectorHome.jsx
+++ hygieno/src/pages/CollectorHome.jsx
@@ -48,7 +48,7 @@ function CollectorHome() {
   },[])
   return (
     <div>
-      <Profile state={userData}></Profile>
+      
        <ServiceSlider sliderData={sliderData} changeServicePage={changeServicePage} icons={icons}></ServiceSlider>
       {
         changeSlider === 0 ? 
